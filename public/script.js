734abd5cd14e82790458ece923aba9d0b4713bf4
hatieudao <trumbien2001@gmail.com>
2020-04-02 16:52:04 +0700

    update:

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const socket = io('http://localhost:3000');
 
 const messageContainer = document.getElementById('message-container');
+const roomContainer = document.getElementById('room-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
@@ -9,9 +10,29 @@ const blue = '#03A696';
 const green = '#84BF04';
 const pink = '#F2C9BB';
 
-const name = prompt('Mời nhập tên');
-appendMessage('You Joined', green);
-socket.emit('new-user', name);
+if(messageForm != null) {
+    const name = prompt('Mời nhập tên');
+    appendMessage('You Joined', green);
+    socket.emit('new-user', roomName, name);
+
+    messageForm.addEventListener('submit', e=>{
+        e.preventDefault();
+        const message = messageInput.value;
+        appendMessage(`You: ${message}`, pink);
+        socket.emit('send-chat-message', roomName,message);
+        messageInput.value = '';
+    });
+}
+
+socket.on('room-created', room => {
+    const roomElement = document.createElement('div');
+    roomElement.innerText = room;
+    const roomLink = document.createElement('a');
+    roomLink.href = `/${room}`;
+    roomLink.innerText = 'join';
+    roomContainer,append(roomElement);
+    roomContainer,append(roomLink);
+})
 
 socket.on('user-connected', name => {
     appendMessage(`${name} connected`, green);
@@ -26,13 +47,7 @@ socket.on('chat-message', data=>{
     appendMessage(`${data.name}: ${data.message}`, blue);
 });
 
-messageForm.addEventListener('submit', e=>{
-    e.preventDefault();
-    const message = messageInput.value;
-    appendMessage(`You: ${message}`, pink);
-    socket.emit('send-chat-message', message);
-    messageInput.value = '';
-});
+
 
 function appendMessage(message, color) {
     const messageElement = document.createElement('div');
